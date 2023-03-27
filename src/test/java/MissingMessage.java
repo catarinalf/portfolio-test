e229b0b438c080e38622000ddc81a7ca598c4d6d
catarinalf <>
2023-03-27 07:33:46 +0100

    Form tests

---
+++ src/test/java/MissingMessage.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class MissingMessage {
+
+    public static void main(String[] args) {
+
+        // If you want to test the code, please make sure to change the path to the MS Edge Driver
+        
+        System.setProperty("webdriver.edge.driver", "D:/edgedriver_win64/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://catarinalf.github.io/#contact");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement nameField = driver.findElement(By.name("name"));
+        nameField.sendKeys("Name");
+
+        WebElement emailField = driver.findElement(By.name("email"));
+        emailField.sendKeys("test@test.com");
+
+        WebElement numberField = driver.findElement(By.name("number"));
+        numberField.sendKeys("123456789");
+
+        WebElement messageField = driver.findElement(By.name("message"));
+        messageField.sendKeys("");
+
+        // Find the submit button and click it
+
+        WebElement button = driver.findElement(By.id("formButton"));
+        button.click();
+        
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Verify if the message is sent without the Name field
+        
+        WebElement requiredField = driver.findElement(By.id("message"));
+
+        String requiredFieldErrorMsg = requiredField.getAttribute("validationMessage");
+    
+        if (requiredFieldErrorMsg.equals("Preencha este campo.")) {
+        System.out.println("Error message was displayed correctly");
+        } 
+        
+        else {
+        System.out.println("Error message was not displayed correctly");
+        }
+
+        driver.quit();
+    }
+
+}
+
---
+++ src/test/java/MissingNumber.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+        // If you want to test the code, please make sure to change the path to the MS Edge Driver
+        
+        System.setProperty("webdriver.edge.driver", "D:/edgedriver_win64/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://catarinalf.github.io/#contact");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement nameField = driver.findElement(By.name("name"));
+        nameField.sendKeys("Name");
+
+        WebElement emailField = driver.findElement(By.name("email"));
+        emailField.sendKeys("test@test.com");
+
+        WebElement numberField = driver.findElement(By.name("number"));
+        numberField.sendKeys("");
+
+        WebElement messageField = driver.findElement(By.name("message"));
+        messageField.sendKeys("Test message");
+
+        // Find the submit button and click it
+
+        WebElement button = driver.findElement(By.id("formButton"));
+        button.click();
+        
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Verify if the message is sent without the Name field
+        
+        Alert alert = driver.switchTo().alert();
+        String alertMessage = alert.getText();
+
+        String expectedMessage = "Sent!";
+        
+        if (alertMessage.equals(expectedMessage)) {
+        System.out.println("Alert message was displayed correctly");
+        } 
+        
+        else {
+        System.out.println("Alert message was not displayed correctly");
+        }
+    }
+
+}
+
